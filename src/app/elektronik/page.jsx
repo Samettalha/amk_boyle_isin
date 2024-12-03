6d020450ac80d0c9c1caf8b2ae8e1bdb03d23303
Samettalha <141047342+Samettalha@users.noreply.github.com>
2024-12-03 23:58:14 +0300

    son push çakışma düzeltildi

---
+++ src/app/elektronik/page.jsx
@@ -0,0 +1,8 @@
+export default function Electronicpage() {
+    return (
+      <div className="w-full h-screen flex flex-col gap-7 items-center justify-center px-5 bg-[url('/images/arkaplan1.jpeg')] bg-cover bg-center">
+        elwktronik sayfası
+      </div>
+    );
+  }
+  
